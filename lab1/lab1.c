ce01486e5a6d600a6b68e1c0badb2be09d46c5b0
Alvin Kong <ask27@sfu.ca>
2025-09-14 04:43:33 -0700

    added some clarifying comments, a header

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -1,7 +1,14 @@
+/* Filename: lab1.c
+ * Description: Program receives a full ine of user (keyboard) input, tokenizes
+ * it with delimiter " " (space), and prints out each token one at a time on a
+ * new line Author: Alvin Kong Date: 2025/09/14
+ */
+
 #define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 int main() {
   // variable initialization
   char *userInput = NULL;
@@ -20,6 +27,8 @@ int main() {
     }
     exit(EXIT_FAILURE);
   }
+
+  // tokenize user input, print out each token
   for (char *inputInit = userInput;; inputInit = NULL) {
     token = strtok_r(inputInit, " ", &savePtr);
     if (token == NULL) {
