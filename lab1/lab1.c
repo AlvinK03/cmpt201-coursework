e14989fe821db742632e51ba9830a03322f231c6
Alvin Kong <ask27@sfu.ca>
2025-09-14 04:35:32 -0700

    Lab 1 Work

---
+++ lab1/lab1.c
@@ -0,0 +1,32 @@
+#define _POSIX_C_SOURCE 200809L
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main() {
+  // variable initialization
+  char *userInput = NULL;
+  char *savePtr = NULL;
+  char *token = NULL;
+  size_t size = 0;
+
+  // receive and store user input
+  printf("Please enter some text: ");
+  ssize_t num_char = getline(&userInput, &size, stdin);
+  // throw error if getline fails
+  if (num_char == -1) {
+    perror("getline failed");
+    if (userInput != NULL) {
+      free(userInput);
+    }
+    exit(EXIT_FAILURE);
+  }
+  for (char *inputInit = userInput;; inputInit = NULL) {
+    token = strtok_r(inputInit, " ", &savePtr);
+    if (token == NULL) {
+      break;
+    }
+    printf("%s\n", token);
+  }
+  free(userInput);
+  return 0;
+}
